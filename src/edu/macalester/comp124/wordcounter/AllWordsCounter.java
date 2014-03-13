7c31a070b4de1fb99abf833f4f35759f07a79f3b
Ned Read <needarb@gmail.com>
2014-03-13 14:23:33 -0500

    Uploaded word-counter Updated Personal

--- src/edu/macalester/comp124/wordcounter/AllWordsCounter.java
+++ src/edu/macalester/comp124/wordcounter/AllWordsCounter.java
@@ -4,62 +4,81 @@ import java.util.Set;
 
 /**
  * A counter that keeps track of counts for all words
- * 
- * @author shilad
  *
+ * @author shilad
  */
-public class AllWordsCounter {
+public class AllWordsCounter
+{
 
     public static final int MAX_WORDS = 10000;
 
-	// TODO: initialize instance variable to hold MAX_WORDS objects
+    // TODO: initialize instance variable to hold MAX_WORDS objects
     SingleWordCounter counters[];
 
-    public int getNumWords() {
-        // TODO: count the number of distinct words,
-        // ie. the number of non-null counter objects.
-        return -1;
+    public AllWordsCounter()
+    {
+        counters = new SingleWordCounter[MAX_WORDS];
     }
-	
-	/**
-	 * Increment the count for the specified word.  Remember that this may
-     * be the first time the word counter has seen this particular word.
-	 * 
-	 * @param word
-	 */
-	public void count(String word) {
-        int n = getNumWords();
-        for (int i = 0; i < n; i++) {
-            // If you find the word increment the count and return
-        }
 
-        // You didn't find the word. Add a new word counter to the array.
-        // Don't forget to increment the word's count to get it to 1!
-	}
-	
-	/**
-	 * Return the count for the particular word.  Remember that the
-	 * word may not have been seen before.
-	 * @param word
-	 * @return
-	 */
-	public int getCount(String word) {
-        // TODO: pattern this after the count() function.
-        // Make sure to return 0 for words you haven't seen before.
-        return -1;
-	}
-	
-	/**
-	 * @return The an array of all words that have been counted
-	 * (just the words, not the values).
-	 */
-	public String []  getAllWords() {
+    /**
+     * @return number of unique words in counters
+     */
+    public int getNumWords()
+    {
+        for (int i = 0; i < counters.length; i++)
+            if (counters[i] == null)
+                return i;
+        return counters.length;
+    }
+
+    /**
+     * Increment the count for the specified word.  Remember that this may
+     * be the first time the word counter has seen this particular word.
+     *
+     * @param word
+     */
+    public void count(String word)
+    {
+        int n = getNumWords();
+        for (int i = 0; i < n; i++)
+        {
+            if (counters[i].wordMatches(word))
+            {
+                counters[i].incrementCount();
+                return;
+            }
+        }
+        (counters[n] = new SingleWordCounter(word)).incrementCount();
+    }
+
+    /**
+     * Return the count for the particular word.  Remember that the
+     * word may not have been seen before.
+     *
+     * @param word
+     * @return
+     */
+    public int getCount(String word)
+    {
+        for(int i=0; i<getNumWords(); i++)
+            if(counters[i].wordMatches(word))
+                return counters[i].getCount();
+        return 0;
+    }
+
+    /**
+     * @return The an array of all words that have been counted
+     * (just the words, not the values).
+     */
+    public String[] getAllWords()
+    {
         // part one: create an array of strings of size equal to the number of words
         int n = getNumWords();
-        String words[] = null;  // FIXME
+        String words[] = new String[n];
 
         // part two: fill the array of strings using a loop
-
+        for(int i = 0; i<n; i++)
+            words[i] = counters[i].getWord();
         return words;
-	}
+    }
 }
--- src/edu/macalester/comp124/wordcounter/DistinctiveWordFinder.java
+++ src/edu/macalester/comp124/wordcounter/DistinctiveWordFinder.java
@@ -9,146 +9,169 @@ import java.util.Arrays;
 /**
  * Interesting word finder.
  * To use this class, first call countWords(), then call findDistinctive.
- * 
+ * <p/>
  * Your tasks are:
  * 1. Complete the countWordsInOneFile method (you only need to add three lines of code).
  * 2. Complete the findInterestingWordsOneWay method (this should be done in 3 stages).
- * 
- * @author shilad
  *
+ * @author shilad
  */
-public class DistinctiveWordFinder {
-	// Word counts from each file.
-	private AllWordsCounter primaryCounts;
-	private AllWordsCounter secondaryCounts;
-	
-	/**
-	 * Constructs a new word counter.
-	 */
-	public DistinctiveWordFinder() {
+public class DistinctiveWordFinder
+{
+    // Word counts from each file.
+    private AllWordsCounter primaryCounts;
+    private AllWordsCounter secondaryCounts;
+
+    /**
+     * Constructs a new word counter.
+     */
+    public DistinctiveWordFinder()
+    {
         primaryCounts = new AllWordsCounter();
         secondaryCounts = new AllWordsCounter();
-	}
-	
-	/**
-	 * Counts the words in two files.
-	 * @param primaryFile
-	 * @param secondaryFile
-	 * @throws java.io.IOException
-	 */
-	public void countWords(File primaryFile, File secondaryFile) throws IOException {
-		countWordsInOneFile(primaryFile, primaryCounts);
-		countWordsInOneFile(secondaryFile, secondaryCounts);
-	}
+    }
 
-	/**
-	 * Counts the words in a single file.  The counts should be tallied
-	 * in the passed-in counts object.
-	 *
-	 * @param file1
-	 * @param counter
-	 * @throws java.io.IOException
-	 */
-	private void countWordsInOneFile(File file1, AllWordsCounter counter) throws IOException {
-		BufferedReader r = new BufferedReader(new FileReader(file1));
-		while (true) {
-			String line = r.readLine();
-			if (line == null) {
-				break;
-			}
-            for (String w : splitLine(line)) {
+    /**
+     * Counts the words in two files.
+     *
+     * @param primaryFile
+     * @param secondaryFile
+     * @throws java.io.IOException
+     */
+    public void countWords(File primaryFile, File secondaryFile) throws IOException
+    {
+        countWordsInOneFile(primaryFile, primaryCounts);
+        countWordsInOneFile(secondaryFile, secondaryCounts);
+    }
+
+    /**
+     * Counts the words in a single file.  The counts should be tallied
+     * in the passed-in counts object.
+     *
+     * @param file1
+     * @param counter
+     * @throws java.io.IOException
+     */
+    private void countWordsInOneFile(File file1, AllWordsCounter counter) throws IOException
+    {
+        BufferedReader r = new BufferedReader(new FileReader(file1));
+        while (true)
+        {
+            String line = r.readLine();
+            if (line == null)
+               break;
+            for (String w : splitLine(line))
                 counter.count(w);
-            }
-		}
-	}
-	
-	/**
-	 * Find and display words in the primary word counter that are interesting.
-	 * Use System.out.println() to print out information about the interesting
-	 * words.
-	 */
-	private void findDistinctive() {
-        // TODO: initialize to words in primary counter
-        String words[] = null;
+        }
+    }
+
+    /**
+     * Find and display words in the primary word counter that are interesting.
+     * Use System.out.println() to print out information about the interesting
+     * words.
+     */
+    private void findDistinctive()
+    {
+        String words[] = primaryCounts.getAllWords();
 
         // This array will be used to sort the word scores;
         WordScore scores[] = new WordScore[words.length];
 
-        for (int i = 0; i < words.length; i++) {
+        for (int i = 0; i < words.length; i++)
+        {
             String word = words[i];
 
-            // TODO: correctly get the primary and secondary counts from
             // the primaryCounts and secondaryCounts instance variables.
-            int primary = 0;
-            int secondary = 0;
+            int primary = primaryCounts.getCount(word);
+            int secondary = secondaryCounts.getCount(word);
 
             // Calculate the interestingness score and prepare it in the output queue
             double score = getDistinctiveScore(primary, secondary);
+           // System.out.println(word + " : " + score);
             scores[i] = new WordScore(word, score);
         }
 
         // Display the most interesting scores.
         Arrays.sort(scores);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++)
+        {
             String word = scores[i].getWord();
             int c1 = primaryCounts.getCount(word);
             int c2 = secondaryCounts.getCount(word);
             System.out.println("word: " + word + ", primary=" + c1 + ", secondary=" + c2);
         }
-	}
+    }
+
+    public static final int K = 1;
 
     /**
      * Returns a "score" indicating how interesting a word is.
-     * @param primaryCount  The count for a specific word for the primary candidate (i.e. Biden)
+     *
+     * @param primaryCount   The count for a specific word for the primary candidate (i.e. Biden)
      * @param secondaryCount The count for a specific word for the secondary candidate (i.e. Palin)
      * @return
      */
-    private double getDistinctiveScore(int primaryCount, int secondaryCount) {
-        // TODO: return something reasonable.
-        // A higher value means more "distinctive".
-        return 0.0;
+    private double getDistinctiveScore(int primaryCount, int secondaryCount)
+    {
+        double score1 = (double) primaryCount / (primaryCount + secondaryCount + K);
+        double score2 = (double) secondaryCount / (primaryCount + secondaryCount + K);
+        //score1 = primaryCount - secondaryCount;
+    //    if(score1 > score2)
+  //          return score2;
+        return score1;
     }
 
-	/**
-	 * Splits a line into words.
-	 * @param line
-	 * @return An array containing the words.
-	 */
-	private String [] splitLine(String line) {
-		return line.split("[^a-zA-Z0-9']+");
-	}
-	
-	public static void main(String args[]) throws IOException {
-        File f1 = new File("dat/palin.txt");
-        File f2 = new File("dat/biden.txt");
+    /**
+     * Splits a line into words.
+     *
+     * @param line
+     * @return An array containing the words.
+     */
+    private String[] splitLine(String line)
+    {
+        return line.split("[^a-zA-Z0-9']+");
+    }
 
-		DistinctiveWordFinder finder = new DistinctiveWordFinder();
-		finder.countWords(f1, f2);
-		finder.findDistinctive();
-	}
+    public static final File f3 = new File("C:\\Users\\needa_000\\IdeaProjects\\word-counter\\dat\\palin.txt");
+    public static final File f4 = new File("C:\\Users\\needa_000\\IdeaProjects\\word-counter\\dat\\biden.txt");
+    public static void main(String args[]) throws IOException
+    {
+        File f1 = new File("/dat/palin.txt");
+        File f2 = new File("/dat/biden.txt");
+        System.out.println(f1.getAbsolutePath());
+
+        DistinctiveWordFinder finder = new DistinctiveWordFinder();
+        finder.countWords(f3, f4);
+        finder.findDistinctive();
+    }
 
     /**
      * Helper class used for sorting word scores.
      */
-    private static class WordScore implements Comparable<WordScore> {
+    private static class WordScore implements Comparable<WordScore>
+    {
         private String word;
         private Double score;
 
-        private WordScore(String word, Double score) {
+        private WordScore(String word, Double score)
+        {
             this.word = word;
             this.score = score;
         }
 
-        private String getWord() {
+        private String getWord()
+        {
             return word;
         }
 
-        private Double getScore() {
+        private Double getScore()
+        {
             return score;
         }
 
         @Override
-        public int compareTo(WordScore wordScore) {
+        public int compareTo(WordScore wordScore)
+        {
             return -1 * score.compareTo(wordScore.getScore());
         }
     }
